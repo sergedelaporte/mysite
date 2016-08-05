9bbaa4b8b7d71e680fbcdbbfc300d6e9eadaf316
sergedelaporte <vic_29@hotmail.fr>
2016-08-05 14:14:48 +0200

    s

--- js/google_map.js
+++ js/google_map.js
@@ -30,20 +30,17 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Brooklyn'];
+    
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
+    
             var latlng = new google.maps.LatLng(48.474409, -4.331345);
             new google.maps.Marker({
                 position: latlng,
                 map: map,
-                icon: 'images/loc.png'
+                //icon: 'images/loc.png'
             });
 
-        });
-    }
+    
     
 }
 google.maps.event.addDomListener(window, 'load', init);
